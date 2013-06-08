1270d5536933cfc0b842f9935f828cdb57f251e6
Ogibayashi Hironori <ogi@Ogibayashi-Hironori-no-MacBook-Pro.local>
2013-06-08 22:37:51 +0900

    Initial commit.

---
+++ lib/fluent/plugin/in_hadoop_metrics.rb
@@ -0,0 +1,11 @@
+require "fluent-plugin-hadoop-metrics/version"
+
+module Fluent
+  module Plugin
+    module Hadoop
+      module Metrics
+        # Your code goes here...
+      end
+    end
+  end
+end
